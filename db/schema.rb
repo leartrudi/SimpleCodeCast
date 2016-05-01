2399ff59491bf4a7ef401bd266ad7fe7645507c0
Rudi on Ruby <leart_rudi@yahoo.com>
2016-05-01 19:56:16 +0000

    Added modified devise for stripe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428132149) do
+ActiveRecord::Schema.define(version: 20160501195106) do
 
   create_table "contacs", force: true do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160428132149) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "plan_id"
+    t.string   "stripe_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
